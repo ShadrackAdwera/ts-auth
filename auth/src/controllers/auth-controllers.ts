71219cb9cdf4ce77c6b6b13ba8a03dae9d80e751
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-10 08:02:31 +0300

    create login controller

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -46,3 +46,42 @@ const signUp = async(req: Request, res: Response, next: NextFunction) => {
     }
     res.status(201).json({message: 'Login Successful', user: { id: newUser.id, email: newUser.email, token: accessToken }});
 }
+
+const login = async(req: Request, res: Response, next: NextFunction) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()) {
+        return next(new HttpError('Invalid inputs', 422));
+    }
+    const { email, password } = req.body;
+
+    let foundUser;
+    let isPassword;
+    let accessToken;
+
+    try {
+        foundUser = await User.findOne({email}).exec();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    if(!foundUser) {
+        return next(new HttpError('This accout don\'t exist! Login instead', 400));
+    }
+
+    try {
+        isPassword = await bcrypt.compare(password, foundUser.password);
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+    if(!isPassword) {
+        return next(new HttpError('Invalid email or password', 401));
+    }
+
+    try {
+        accessToken = generateAccessTokens(foundUser);
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    res.status(200).json({message: 'Login Successful', user: { id: foundUser.id, email: foundUser.email, token: accessToken }});
+
+ }
