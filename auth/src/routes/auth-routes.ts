14e07796f503d319584a0a4dc0468c853a5f6b66
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-10 08:07:08 +0300

    create auth routes

---
+++ auth/src/routes/auth-routes.ts
@@ -0,0 +1,19 @@
+import { body } from 'express-validator';
+import express from 'express';
+
+import { signUp, login } from '../controllers/auth-controllers';
+
+const router = express.Router();
+
+router.post('/sign-up', [
+    body('username').trim().not().isEmpty().isLength({ min: 3 }),
+    body('email').trim().normalizeEmail().isEmail(),
+    body('password').trim().not().isEmpty().isLength({ min: 6 })
+], signUp);
+
+router.post('/login', [
+    body('email').trim().normalizeEmail().isEmail(),
+    body('password').trim().not().isEmpty().isLength({ min: 6 })
+], login);
+
+export { router as authRoutes };
