4532ced11f84ad5abce94ed7418aa11060b45292
ShadrackAdwera <adweshshaddie@gmail.com>
2022-03-11 07:34:06 +0300

    modify user model

--- auth/src/models/User.ts
+++ auth/src/models/User.ts
@@ -5,19 +5,25 @@ interface UserDoc extends Document {
     username: string;
     email: string;
     password: string;
+    resetToken?: string;
+    tokenExpiration?: Date;
     version: number;
 }
 
 interface UserModel extends Model<UserDoc> {
     username: string;
     email: string;
+    resetToken?: string;
+    tokenExpiration?: Date;
     password: string;
 }
 
 const userSchema = new Schema({
     username: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true, min: 6 }
+    password: { type: String, required: true, min: 6 },
+    resetToken: { type: String },
+    tokenExpirationDate: { type: Date },
 }, { timestamps: true, toJSON: { getters: true } });
 
 userSchema.set('versionKey', 'version');
