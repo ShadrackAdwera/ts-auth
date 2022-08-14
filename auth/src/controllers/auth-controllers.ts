93f2cb766a03c4b527c77f6ecca9d833437f9e77
ShadrackAdwera <adweshshaddie@gmail.com>
2022-08-14 20:36:40 +0300

    clea up on aisle 3

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -1,20 +1,20 @@
-import { HttpError } from "@adwesh/common";
-import { Request, Response, NextFunction } from "express";
-import { validationResult } from "express-validator";
-import bcrypt from "bcryptjs";
-import brypto from "crypto";
+import { HttpError } from '@adwesh/common';
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import brypto from 'crypto';
 
-import { User } from "../models/User";
+import { User } from '../models/User';
 import {
   generateAccessTokens,
   generateRefreshTokens,
   decodeRefreshToken,
-} from "../utils/generatetokens";
+} from '../utils/generatetokens';
 
 const signUp = async (req: Request, res: Response, next: NextFunction) => {
   const error = validationResult(req);
   if (!error.isEmpty()) {
-    return next(new HttpError("Invalid inputs", 422));
+    return next(new HttpError('Invalid inputs', 422));
   }
   const { username, email, password } = req.body;
 
@@ -26,16 +26,16 @@ const signUp = async (req: Request, res: Response, next: NextFunction) => {
   try {
     foundUser = await User.findOne({ email }).exec();
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   if (foundUser) {
-    return next(new HttpError("This user exists, login instead", 400));
+    return next(new HttpError('This user exists, login instead', 400));
   }
 
   try {
     hashedPassword = await bcrypt.hash(password, 12);
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   const newUser = new User({
     username,
@@ -47,7 +47,7 @@ const signUp = async (req: Request, res: Response, next: NextFunction) => {
   try {
     await newUser.save();
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   try {
     accessToken = generateAccessTokens({
@@ -59,25 +59,23 @@ const signUp = async (req: Request, res: Response, next: NextFunction) => {
       email: newUser.email,
     });
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
-  res
-    .status(201)
-    .json({
-      message: "Login Successful",
-      user: {
-        id: newUser.id,
-        email: newUser.email,
-        token: accessToken,
-        refreshToken,
-      },
-    });
+  res.status(201).json({
+    message: 'Sign Up Successful',
+    user: {
+      id: newUser.id,
+      email: newUser.email,
+      token: accessToken,
+      refreshToken,
+    },
+  });
 };
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
   const error = validationResult(req);
   if (!error.isEmpty()) {
-    return next(new HttpError("Invalid inputs", 422));
+    return next(new HttpError('Invalid inputs', 422));
   }
   const { email, password } = req.body;
 
@@ -89,7 +87,7 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
     foundUser = await User.findOne({ email }).exec();
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   if (!foundUser) {
     return next(new HttpError("This accout don't exist! Login instead", 400));
@@ -98,11 +96,11 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
     isPassword = await bcrypt.compare(password, foundUser.password);
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
 
   if (!isPassword) {
-    return next(new HttpError("Invalid email or password", 401));
+    return next(new HttpError('Invalid email or password', 401));
   }
 
   try {
@@ -115,19 +113,17 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
       email: foundUser.email,
     });
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
-  res
-    .status(200)
-    .json({
-      message: "Login Successful",
-      user: {
-        id: foundUser.id,
-        email: foundUser.email,
-        token: accessToken,
-        refreshToken,
-      },
-    });
+  res.status(200).json({
+    message: 'Login Successful',
+    user: {
+      id: foundUser.id,
+      email: foundUser.email,
+      token: accessToken,
+      refreshToken,
+    },
+  });
 };
 
 const requestPasswordReset = async (
@@ -137,7 +133,7 @@ const requestPasswordReset = async (
 ) => {
   const error = validationResult(req);
   if (!error.isEmpty()) {
-    return next(new HttpError("Invalid email", 422));
+    return next(new HttpError('Invalid email', 422));
   }
 
   const { email } = req.body;
@@ -146,13 +142,13 @@ const requestPasswordReset = async (
   try {
     foundUser = await User.findOne({ email }).exec();
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   if (!foundUser) {
-    return next(new HttpError("Invalid email", 404));
+    return next(new HttpError('Invalid email', 404));
   }
 
-  const resetTkn = brypto.randomBytes(64).toString("hex");
+  const resetTkn = brypto.randomBytes(64).toString('hex');
   const tokenExpDate = new Date(new Date().getTime() + 3600000);
 
   foundUser.resetToken = resetTkn;
@@ -161,14 +157,14 @@ const requestPasswordReset = async (
   try {
     await foundUser.save();
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   console.log(`http://localhost:3000/reset-password/${resetTkn}`);
 
   //send email with reset link ${frontend_url}/resetToken
   res
     .status(200)
-    .json({ message: "Check your email for a reset password link" });
+    .json({ message: 'Check your email for a reset password link' });
 };
 
 const resetPassword = async (
@@ -183,7 +179,7 @@ const resetPassword = async (
   let foundUser;
   //check if passwords match
   if (password !== confirmPassword) {
-    return next(new HttpError("The passwords do not match", 422));
+    return next(new HttpError('The passwords do not match', 422));
   }
 
   try {
@@ -192,17 +188,17 @@ const resetPassword = async (
       tokenExpiration: { $gt: new Date() },
     });
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
 
   if (!foundUser) {
-    return next(new HttpError("Invalid request", 403));
+    return next(new HttpError('Invalid request', 403));
   }
 
   try {
     hashedPassword = await bcrypt.hash(password, 12);
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   foundUser.password = hashedPassword;
   foundUser.tokenExpiration = undefined;
@@ -211,12 +207,12 @@ const resetPassword = async (
   try {
     await foundUser.save();
   } catch (error) {
-    return next(new HttpError("An error occured, try again", 500));
+    return next(new HttpError('An error occured, try again', 500));
   }
   // invalidate any refresh tokens / access tokens
   res
     .status(200)
-    .json({ message: "Your password has been successfully reset" });
+    .json({ message: 'Your password has been successfully reset' });
 };
 
 const generateNewTokens = async (
@@ -230,7 +226,7 @@ const generateNewTokens = async (
   const { email, userId } = await decodeRefreshToken(refreshToken);
 
   if (userId !== reqUserId) {
-    return next(new HttpError("Forbidden request", 403));
+    return next(new HttpError('Forbidden request', 403));
   }
   const newAccessToken = generateAccessTokens({ id: userId, email });
   const newRefreshToken = generateRefreshTokens({ id: userId, email });
