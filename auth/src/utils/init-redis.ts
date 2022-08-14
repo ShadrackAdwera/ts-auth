0bc1fbd177fbf16287c4667fb4fd7021bb18b0c8
ShadrackAdwera <adweshshaddie@gmail.com>
2022-08-14 20:06:39 +0300

    reconfigure redis singleton

--- auth/src/utils/init-redis.ts
+++ auth/src/utils/init-redis.ts
@@ -1,28 +1,24 @@
 import { createClient, RedisClientType } from 'redis';
 
 class RedisClient {
-    private _client?: RedisClientType;
+  private _client?: RedisClientType;
 
-    get client() {
-        if(!this._client) {
-            throw new Error('REDIS must be initialised');
-        }
-        return this._client;
+  get client() {
+    if (!this._client) {
+      throw new Error('REDIS must be initialised');
     }
+    return this._client;
+  }
 
-    connect(url: string) {
-        this._client = createClient({ url });
-        return new Promise<void>((resolve, reject)=>{
-            this.client.on('connect', ()=>{
-                console.log('Connected to REDIS');
-                resolve();
-            });
-            this.client.on('error', (err)=>{
-                reject(err);
-            });
-        });
-    }
-
+  connect(url: string): Promise<void> {
+    this._client = createClient({ url });
+    return this.client
+      .connect()
+      .then(() => {
+        console.log('Connected to REDIS');
+      })
+      .catch((error) => console.log(error));
+  }
 }
 
 export const initRedis = new RedisClient();
